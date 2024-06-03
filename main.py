7256f805d2067dc0e2d16a93e02e3d9903670c5b
coopstev <coopstev@umich.edu>
2024-06-03 03:04:11 +0000

    Condensed list of SP500 symbols into SP500List.txt

--- main.py
+++ main.py
@@ -2,13 +2,15 @@ from DataRetreiver import DataRetreiver
 from DataRequester import DataRequester
 from Emailer import Emailer
 from Opener import Opener
+from StateTracker import StateTracker
 import time
 
 # startup:
-requester = DataRequester("sp500.csv", ['RSI'])
+requester = DataRequester("SP500List.txt", ['RSI'])
 retreiver = DataRetreiver()
 emailer = Emailer()
 opener = Opener()
+tracker = StateTracker()
 
 # start gui
 
@@ -18,6 +20,8 @@ opener = Opener()
 while opener.isOpen():
     request = requester.getRequest()
     data = retreiver.getData(request)
+    tracker.getChanges(data)
+    
     if data < 30:
         emailer.send_email()
     elif data > 70:
