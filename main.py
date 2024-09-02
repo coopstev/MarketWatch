eb38385c28fb34d515939d695de855442b4c2987
coopstev <coopstev@umich.edu>
2024-09-02 02:35:45 +0000

    Modify main.py to reflect the changes to the respective functional classes.

--- main.py
+++ main.py
@@ -3,14 +3,17 @@ from DataRequester import DataRequester
 from Emailer import Emailer
 from Opener import Opener
 from StateTracker import StateTracker
+from Notifier import Notifier
 import time
 
 # startup:
-requester = DataRequester("SP500List.txt", ['RSI'])
+requester = DataRequester("SP500List.txt", ['RSI'], 20)
+symbols = requester.getAllSymbols()
 retreiver = DataRetreiver()
-emailer = Emailer()
+emailer = Emailer(["coopstev012@gmail.com", "lilypad22003@gmail.com"])
+notifier = Notifier()
 opener = Opener()
-tracker = StateTracker()
+tracker = StateTracker(symbols)
 
 # start gui
 
@@ -20,15 +23,20 @@ tracker = StateTracker()
 while opener.isOpen():
     request = requester.getRequest()
     data = retreiver.getData(request)
-    tracker.getChanges(data)
+    tracker.logChanges(data)
+    if notifier.isTimeToSendNotification():
+        notifier.sendNotification(emailer, tracker)
     
-    if data < 30:
-        emailer.send_email()
-    elif data > 70:
-        emailer.send_email()
-    time.sleep(12)
+    #if data < 30:
+        #emailer.send_email()
+    #elif data > 70:
+        #emailer.send_email()
+    time.sleep(1)
 
 # now the market is closed
+
+# send an end-of-day notification
+notifier.sendNotification(emailer, tracker)
 
 #stop gui
 
