629176cf6f2be71872e6819abfc0327286902034
coopstev <coopstev@umich.edu>
2024-09-02 02:19:29 +0000

    Create test script for DataRetriever functionality.

---
+++ testRetreiver.py
@@ -0,0 +1,15 @@
+from DataRetreiver import DataRetreiver
+from datetime import datetime
+
+
+def convertToDate(timeInms):
+    date = datetime.fromtimestamp(timeInms // 1000)
+    return date
+
+retreiver = DataRetreiver()
+rsi = retreiver.getData([("AAPL", "RSI")])
+print(rsi)
+for iv in rsi[0].values:
+    print(f"{convertToDate(iv.timestamp)}")
+for iv in rsi[0].values:
+    print(f"{iv.value}")
