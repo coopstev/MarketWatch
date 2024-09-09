c2b70ce18a3102dd8b9cf1846b1d2a14f5aef574
coopstev <coopstev@umich.edu>
2024-09-09 18:06:04 +0000

    Correction: Decrease time between notifications to 30mins from 333mins.

--- Notifier.py
+++ Notifier.py
@@ -14,7 +14,7 @@ STATE_HEADERS = { RSIState.HARDSELL : "HARD SELL (75 <= RSI)",
                   RSIState.HARDBUY  : "HARD BUY (RSI <= 25)"
                 }
 
-TIME_BETWEEN_NOTIFICATIONS_SECS = 333* 60  # 10 minutes
+TIME_BETWEEN_NOTIFICATIONS_SECS = 30 * 60  # 10 minutes
 
 class Notifier:
     def __init__(self, debug):
