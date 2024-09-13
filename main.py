870fbe32eb07d1162d641e022270a6f1ca26f723
Cooper Stevens <coopstev@umich.edu>
2024-09-12 20:37:13 -0500

    Add Purchaser functionality utilization to main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from Emailer import Emailer
 from Opener import Opener
 from StateTracker import StateTracker
 from Notifier import Notifier
+from Purchaser import Purchaser
 import time
 
 RSI = "RSI"
@@ -16,12 +17,20 @@ ADMIN = ["coopstev012@gmail.com"]
 SUBSCRIBERS = ["rhino9161972@yahoo.com"]
 
 PURCHASER_ON = True
+DAILY_RSI_MODEL = "1dRSI"
+MINUTELY_RSI_MODEL = "1mRSI"
+PRICE = "PRICE"
 
 # startup:
 requester = DataRequester("SP500List.txt", [RSI], 120)
 symbols = requester.getAllSymbols()
-retriever = DataRetriever("1d", DEBUG)
-if PURCHASER_ON : minuteRetriever = DataRetriever("1m", DEBUG)
+retriever = DataRetriever("1d", symbols, DEBUG)
+
+if PURCHASER_ON :
+    minuteRetriever = DataRetriever("1m", symbols, DEBUG)
+    daily = Purchaser(DAILY_RSI_MODEL, { PRICE : retriever , "1d" : retriever }, True)
+    minutely = Purchaser(MINUTELY_RSI_MODEL, { PRICE : retriever , "1m" : minuteRetriever }, True)
+
 emailer = Emailer(ADMIN if DEBUG else ADMIN + SUBSCRIBERS)
 notifier = Notifier(DEBUG)
 opener = Opener(DEBUG)
@@ -33,8 +42,10 @@ if PURCHASER_ON : minuteTracker = StateTracker(symbols)
 while opener.isBeforeOpen():
     time.sleep(60)
 
-while opener.isOpen():
+isOpen = opener.isOpen()
+while isOpen:
     request = requester.getRequest()
+    requestedSymbols = [ symbol for symbol, metric in request ]
 
     data = retriever.getData(request)
     if PURCHASER_ON : minuteData = minuteRetriever.getData(request)
@@ -47,11 +58,23 @@ while opener.isOpen():
         if sentSuccess : tracker.updateStates()
     
     if PURCHASER_ON:
-        
+        daily.strategy(requestedSymbols, data[RSI])
+        minutely.strategy(requestedSymbols, minuteData[RSI])
 
     time.sleep(1)
+    isOpen = opener.isOpen()
+
 
 # now the market is closed
+
+if PURCHASER_ON:
+    daily.overwriteHoldings()
+    daily.writeNewTransactionsToLedger()
+    daily.saveStatement()
+
+    minutely.overwriteHoldings()
+    minutely.writeNewTransactionsToLedger()
+    minutely.saveStatement()
 
 # send an end-of-day notification
 notifier.sendNotification(emailer, tracker, HTML_MESSAGE)
